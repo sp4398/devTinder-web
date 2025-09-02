2afe6bf14c81b5a3b2a1dc661655c64db81affca
Saurav Pandey <sp4398@gmail.com>
2025-09-02 23:34:38 +0530

    feat: implement Chat component with message display and input functionality

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Chat = () => {
   const { targetedUserId } = useParams();
-  return <div></div>;
+  const [messages, setMessages] = useState([{ text: "hello" }]);
+  return (
+    <div className="w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
+      <h1 className="p-1 border-b border-gray-600">Chat</h1>
+      <div className="flex-1 overflow-scroll p-5">
+        {messages.map((msg, idx) => {
+          return (
+            <div key={idx} className="chat chat-start">
+              <div className="chat-header">
+                Saurav
+                <time className="text-xs opacity-50">2 hours ago</time>
+              </div>
+              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-footer opacity-50">Seen</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="p-5 border-t border-gray-600 flex items-center gap-2">
+        <input className="flex-1 border border-gray-500 text-white rounded p-2"></input>
+        <button className="btn btn-secondary">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
