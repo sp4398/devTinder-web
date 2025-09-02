563eae541fc1e263df244d14d1207094ae18dab8
Saurav Pandey <sp4398@gmail.com>
2025-09-02 23:07:05 +0530

    feat: add Chat component and link from Connections

---
+++ src/components/Chat.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const Chat = () => {
+  const { targetedUserId } = useParams();
+  return <div></div>;
+};
+
+export default Chat;
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -3,6 +3,7 @@ import { BASE_URL } from "../utils/constants";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addConnections } from "../utils/connectionSlice";
+import { Link } from "react-router-dom";
 
 const Connections = () => {
   const connections = useSelector((store) => store.connections);
@@ -14,8 +15,8 @@ const Connections = () => {
         withCredentials: true,
       });
       dispatch(addConnections(res?.data?.data));
-    } catch (error) {
-      //
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -58,6 +59,9 @@ const Connections = () => {
               <p>{about}</p>
               {age && gender && <p>{age + ", " + gender}</p>}
             </div>
+            <Link to={"/chat/" + _id}>
+              <button className="btn btn-primary">Chat</button>
+            </Link>
           </div>
         );
       })}
